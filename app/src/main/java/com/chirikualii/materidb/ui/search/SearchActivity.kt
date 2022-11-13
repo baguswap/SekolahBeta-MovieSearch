492402f8f92e437d603c8db6d3a4dadc4cbab3e3
Lab-ERP-23\Lab-ERP23 <bagushot@gmail.com>
2022-11-13 17:03:21 +0700

    commit

--- app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
+++ app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
@@ -2,9 +2,12 @@ package com.chirikualii.materidb.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Adapter
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModel
 import com.chirikualii.materidb.data.model.Movie
 import com.chirikualii.materidb.databinding.ActivitySearchBinding
 import com.chirikualii.materidb.ui.adapter.MovieListAdapter
@@ -13,6 +16,11 @@ class SearchActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySearchBinding
     private lateinit var listAdapter: MovieListAdapter
+
+    val viewModel : SearchViewModel by viewModels<SearchViewModel>(
+        factoryProducer = { SearchViewModelFactor(context = this)}
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
@@ -21,37 +29,25 @@ class SearchActivity : AppCompatActivity() {
 
         listAdapter = MovieListAdapter()
         binding.rvMovie.adapter = listAdapter
-        listAdapter.addItem(
 
-            listOf(
-                Movie(
-                    movieId = "asd" ,
-                    title = "er",
-                    releaseDate = "ads",
-                    imagePoster = "wewe",
-                    overview = "asda",
-                    backdrop = "adas",
-                    bookmark = 1),
-                Movie(
-                    movieId = "asd" ,
-                    title = "er",
-                    releaseDate = "ads",
-                    imagePoster = "wewe",
-                    overview = "asda",
-                    backdrop = "adas",
-                    bookmark = 1),
-                Movie(
-                    movieId = "asd" ,
-                    title = "er",
-                    releaseDate = "ads",
-                    imagePoster = "wewe",
-                    overview = "asda",
-                    backdrop = "adas",
-                    bookmark = 1),
-            )
-        )
+        oberserveView()
         binding.etSearch.doOnTextChanged { text, _, _, _ ->
+            viewModel.doSearchMovie(text.toString())
             Toast.makeText(this, "searching ${text}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun oberserveView () {
+        viewModel.listMovie.observe(this){
+            listAdapter.addItem(it)
+        }
+
+        viewModel.isLoading.observe(this) {
+            if(it){
+                binding.progressBar.visibility = View.VISIBLE
+            }else {
+                binding.progressBar.visibility = View.INVISIBLE
+            }
         }
     }
 }
